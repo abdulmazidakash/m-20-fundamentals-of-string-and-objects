50f5441ef4c6277072f37d2a8d17651ea3cf6e83
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-02 23:09:14 +0600

    19-1, 19-2, 19-3 string slice,join,concat and includes

---
+++ case.js
@@ -0,0 +1,24 @@
+const school = 'RAJ UK Uttara Model School';
+
+// console.log(school);
+
+const subject = 'Chemistry';
+const book =  'chemistry';
+
+if (subject.toLowerCase === book.toUpperCase) {
+	console.log('I am reading book aibar porikka patai felmu');
+}
+else {
+	console.log('hudai hudai prista ultai ar vat khawar jonno');
+}
+
+const drink = 'water';
+const liquid = 'water';
+
+if (drink === liquid){
+	console.log('pani er opor nam life');
+}
+else {
+	console.log('somudre pani ase khaite pari na');
+}
+
---
+++ string.js
@@ -0,0 +1,25 @@
+const country  = 'bangladesh';
+const division = "new khale";
+const district = `b-baria`;
+// const thana = new String ('Demra');
+
+
+// console.log(typeof thana);
+
+const numbers = [12, 32, 22, 23, 42, 65, 56];
+console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[1]);
+numbers[1] = 11;
+console.log(numbers);
+
+
+// String is immutable --> not changeable
+// array is mutable --> you can change the array elements 
+
+const capital ='dhaka';
+// console.log(capital.length);
+// console.log(capital[2]);
+console.log(capital);
+capital[1] = 'q';
+console.log(capital);
