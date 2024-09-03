4bda334d9cafaa96e1c0ab050d7509468aad0164
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-03 07:30:41 +0600

    20-5 introduction to objects properties and values

---
+++ object.js
@@ -0,0 +1,34 @@
+//primitive
+//basic, primary
+
+const age = 21;
+const school = 'rifle square';
+const isPassed = true;
+//let isDeveloper;
+
+const subjects = ['bangla', 'english', 'physics', 'math'];
+const bottle_wrong  = ['white', 45];
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+//object
+const bottle = {
+	brand: 'apple',
+	price: 45,
+	color: 'white',
+	isClean: false
+}
+
+
+//non-primitive
+const subject = {
+	namek: 'biology',
+	teacher: 'rasheda mam',
+	examDate: '30 Feb',
+	chapters: ['first', 'second', 'third'],
+	exams: {
+		name: 'Final Exam',
+		marks: 100
+	}
+}
