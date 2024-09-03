9d8b4d539a435344370c4dccd3268b43079409a8
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-03 07:46:06 +0600

    20-6 what is object animated video

---
+++ object_animated.js
@@ -0,0 +1,31 @@
+let cat = {
+	name: 'cat',
+	age: 2,
+	favouriteFish: 'pangas',
+}
+
+const objppect_example = {
+	key1: 'value1',
+	key2: 'value2',
+	key3: 'value3'
+};
+
+//object example:
+
+const car = {
+	model: 'bmw',
+	color: 'black',
+	price: 250000
+};
+
+//object method:
+const car1 = {
+	model: 'bmw',
+	color: 'black',
+	price: 250000,
+	accelerate: function() {
+		console.log('car speed  increase!');
+	}
+};
+
+car1.accelerate(); //car speed increase print
